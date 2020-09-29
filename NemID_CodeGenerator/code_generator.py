32d23b7dc21ed32600a6c25f0e8512878f880932
Jakob <wulff@nyxapp.net>
2020-09-29 15:41:07 +0200

    eow

---
+++ NemID_CodeGenerator/code_generator.py
@@ -0,0 +1,55 @@
+from flask import request, Response, Flask
+import json
+from db_handler import authenticate_nem_id, generate_auth_log
+from random import randint
+
+
+app = Flask(__name__)
+app.config["DEBUG"] = True
+
+
+@app.route('/nemid-auth', methods=['POST'])
+def api_code_generator():
+
+    # parse body to json
+    nem_id_entity = json.loads(request.data)
+
+    # exstract data
+    nem_id_code = nem_id_entity['nemIdCode']
+    nem_id = nem_id_entity['nemId']
+
+    # authenticate user
+    user_id = authenticate_nem_id(nem_id, nem_id_code)
+
+    if user_id is None:
+        # create response body
+        response_body = {
+            "status": "Invalid credentials",
+            "error_message" : "The provided NemID and Password does not belong to any user"
+        }
+
+        # create response
+        response = Response()
+        response.status_code = 401
+        response.data = json.dumps(response_body)
+        return response
+    else:
+        # generate random 6 digit code 
+        code = f"{randint(0,9)}{randint(0,9)}{randint(0,9)}{randint(0,9)}{randint(0,9)}{randint(0,9)}"
+
+        # generate auth log
+        generate_auth_log(user_id, code)
+
+        # create response body
+        response_body = {
+            "generatedCode": code
+        }
+
+        # create response
+        response = Response()
+        response.status_code = 200
+        response.data = json.dumps(response_body)
+        return response
+
+# begin server
+app.run(port = 8090)
